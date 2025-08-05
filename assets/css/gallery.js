9082496f96f2e245b5ac2d9232fe1de65c3482f2
SuciuTudor <suciutudoralex@gmail.com>
2025-08-05 23:10:39 +0300

    Delete assets/css/gallery.js

--- assets/css/gallery.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.gallery img').forEach(img => {
-    img.addEventListener('click', () => {
-        const fullscreen = document.createElement('div');
-        fullscreen.classList.add('fullscreen');
-        fullscreen.innerHTML = `<img src="${img.src}" alt=""><span class="close">&times;</span>`;
-        document.body.appendChild(fullscreen);
-        fullscreen.querySelector('.close').onclick = () => fullscreen.remove();
-    });
-});
